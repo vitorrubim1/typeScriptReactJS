0d28e8a1652449ae7b522fe13494a6fe162e92ce
Vitor <vitorrubim1@hotmail.com>
2020-05-21 16:30:48 -0300

    feat: configure REDUCER

--- src/store/ducks/repositories/index.ts
+++ src/store/ducks/repositories/index.ts
@@ -1,1 +1,29 @@
 //AQ VAI O REDUCER
+import { Reducer } from 'redux'; //VARIAVEL DE TYPAGEM
+import { RepositoriesState, RepositoriesTypes } from './types'; //IMPORTANDO O ESTADO DOS REPOS
+
+
+const INITIAL_STATE: RepositoriesState = {
+    data: [],
+    error: false,
+    loading: false,
+}
+
+//Inicializando o REDUCER
+const reducer: Reducer<RepositoriesState> = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case RepositoriesTypes.LOAD_REQUEST:
+            return { ...state, loading: true }; //SE FOR REQ, VOU COPIAR TODO O ESTADO E DAR LOADING TRUE
+        case RepositoriesTypes.LOAD_SUCCES:
+            return { ...state, loading: false, error: false, data: action.payload.data }
+        case RepositoriesTypes.LOAD_FAILURE:
+            return { ...state, loading: false, error: true, data: [] };
+        default:
+            return state;
+    }
+}
+/*
+    'Reducer' recebe um parametro, para saber o formato que vai manipular os dados
+*/
+
+
